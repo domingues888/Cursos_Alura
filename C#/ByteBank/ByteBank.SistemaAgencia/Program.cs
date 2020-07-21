086fad72ce86a75dc7520f0d56a3445e1ad560a0
Flávio Domingues <domingues888@gmail.com>
2020-07-20 22:06:06 -0300

    Finalização parte #4 Curso de C#

--- C#/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ C#/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -11,9 +13,14 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(374, 3627362);
-            Console.WriteLine(conta.Numero);
+            DateTime dataFimPagamento = new DateTime(2018, 6, 20);
+            DateTime dataCorrente = DateTime.Now;
 
+            TimeSpan diferenca = TimeSpan.FromMinutes(40); //dataFimPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(mensagem);
 
             Console.ReadLine();
         }
